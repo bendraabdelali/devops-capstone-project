15aed4e4bdd915ecd30e1c2bf0ff245f38f123fd
abdo <abdo@gmail.com>
2022-11-11 09:19:58 -0500

    add select tests case

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,22 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+@app.route("/accounts/<id>", methods=["GET"])
+def read_account(id):
+    """
+        Reads an Account
+        This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", id)
+    
+    account = Account.find(id)
+
+    if not account : 
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] could not be found.")
+    
+    account = account.serialize()
+    
+    return account, status.HTTP_200_OK
 
 
 ######################################################################
